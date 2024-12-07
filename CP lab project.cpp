e56c9e591d6f39f0596fa074363369a8c5e82940
dell <dell@DESKTOP-TE1B7VR>
2024-12-07 15:42:45 +0500

    Add project files.

---
+++ CP lab project.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+const int  rooms = 10;
+int floor_one[rooms];
+int floor_two[rooms];
+int floor_three[rooms];
+int store_room( )
+{
+	char book;22
+}
+int main()
+{
+	cout << "1. ";
+	
+
+}
+
