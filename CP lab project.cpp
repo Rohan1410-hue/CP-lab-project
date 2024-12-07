a6e725c916fecc0f767831c264c406788da8e01a
Rohan1410-hue <ghanirohan21@gmail.com>
2024-12-07 16:53:32 +0500

    Update CP lab project.cpp

--- CP lab project.cpp
+++ CP lab project.cpp
@@ -4,14 +4,28 @@ const int  rooms = 10;
 int floor_one[rooms];
 int floor_two[rooms];
 int floor_three[rooms];
+int bookings()
+{
+	cout<<"1. Vip booking $25000 per room\n";
+	cout<<"2. Normal booking $10000 per room\n";
+	
+}
 int store_room( )
 {
-	char book;22
+	char book;
 }
 int main()
 {
-	cout << "1. ";
-	
-
+	cout<<"1. Staff\n";
+	cout<<"2. Guest\n";
+	int choice;
+	cout<<"Choice:";
+	cin>>choice;
+	switch(choice)
+		{
+			case 1:
+			case 2:
+			
+			
+		}
 }
-
