01761f12ee2482daae6fe2efbe52dd45e7e4fbed
theoaltic <141176722+theoaltic@users.noreply.github.com>
2024-12-09 15:03:16 +0500

    Update CP lab project.cpp

--- CP lab project.cpp
+++ CP lab project.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
+
 const int  rooms = 10;
 int floor_one[rooms];
 int floor_two[rooms];
 int floor_three[rooms];
-int bookings()
-{
-	cout<<"1. Vip booking $25000 per room\n";
-	cout<<"2. Normal booking $10000 per room\n";
-	
+
+int bookings(){
+ cout<<"1. Vip booking $25000 per room\n";
+ cout<<"2. Normal booking $10000 per room\n";
 }
-int store_room( )
-{
+
+int store_room( ){
 	char book;
 }
-int main()
-{
+
+int main(){
 	cout<<"1. Staff\n";
 	cout<<"2. Guest\n";
 	int choice;
