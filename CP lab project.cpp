a98647790ffe8e9cac3b1fa73a6d8d354ec4a5b5
Rohan1410-hue <ghanirohan21@gmail.com>
2024-12-29 22:55:31 +0500

    Update CP lab project.cpp

--- CP lab project.cpp
+++ CP lab project.cpp
@@ -7,25 +7,29 @@
 #include<sstream>
 using namespace std;
 int current_guest = 0;
-fstream current_record,history, current_record_new,meal;
+fstream current_record,history, current_record_new,meal,database;
 int customer_booked = 0;
+int price_per_room = 10000;
 const int  rooms = 108;
 const int  floors = 8;
 const int rooms_per_floor=14;
 const int penthouse_floors = 2;
 const int penthouse_rooms = 12;
+const int max_guests = 100;
+const float room_rate  = 50000.2;
+const float service_fee = 800.1;
 string room_data[floors - 2][rooms_per_floor];
 struct room {
 	int floor;
+	int room;
 	bool booking;	
-}arr_room[rooms],penthouse_arr[penthouse_floors][penthouse_rooms];
+}arr_room[floors][rooms_per_floor],penthouse_arr[penthouse_floors][penthouse_rooms];
 struct customer {
 	int customer_id = rand() % 10000;
 	string customer_name;
 	int time_duration;
-	/*bool park;
-	int num_park;*/
-}arr_customer[rooms];
+	float meal_charges;
+}guests[max_guests],arr_customer[rooms];
 //string funct(int i,int j)
 //{
 //	string *prt_one = &room_data[i][j];
@@ -33,125 +37,178 @@ struct customer {
 //	cout << "Booking completed successfully!\n";
 //	return prt_one;
 //}
+void initializerooms()
+{
+	cout << "Available rooms are:\n";
+	for (int i = 0;i < floors - 2;i++) {
+		for (int j = 0;j < rooms_per_floor;j++) {
+			arr_room[i][j].floor = i + 1;
+			arr_room[i][j].room = j + 1;
+			arr_room[i][j].booking = false;
+		}
+	}
+}
 void check_room_availability()
 {
+	cout << "Available rooms are:\n";
 	for (int i = 0;i < floors - 2;i++) {
-		cout << "Floor " << i + 1 << ": ";
+		cout << "Floor:" << i + 1 << " ";
 		for (int j = 0;j < rooms_per_floor;j++) {
-			cout << room_data[i][j] << setw(10);
+			if (arr_room[i][j].booking == true) arr_room[i][j].room = 0;
+			cout << arr_room[i][j].room<<" ";
 		}
 		cout << endl;
 	}
 }
 void room_book()
 {
-	int j = 0, i = 0;
-	cout << "Available rooms are:\n";
-	for (int i = 0;i < floors-2;i++) {
-		cout << "Floor:"<<i+1<<" ";
-		for (int j = 0;j < rooms_per_floor;j++) {
-			room_data[i][j] = to_string(j + 1);
-			cout << room_data[i][j]<<" ";
-		}
-		cout << endl;
-	}
-cout << endl;
 	for (int i = 0;i <=current_guest;i++)
 	{
 		if (i == current_guest)
 		{
-			int floor;
+			int floor_input,room_input;
 			cout << "Enter floor from 1 to 6:";
-			cin >> floor;
+			cin>> floor_input;
 			cout << "Enter your room :\n";
-			int select_room;
-			cin >> select_room;
-			if (floor > 6 || (select_room > 14 || select_room < 1)) {
+			cin >> room_input;
+			if (floor_input >floors || room_input > rooms_per_floor|| room_input < 1) {
 				cout << "Wrong floor!\n";
 			}
+			if (arr_room[floor_input - 1][room_input - 1].booking==true) {
+				cout << "Room already booked.\n";
+			}
 			else {
-				arr_room[i].booking = true;
+				arr_room[floor_input - 1][room_input - 1].booking = true;
+				cout << "Room booked successfully.\n";
+			}
+			/*else {
+				arr_room.booking = true;
 				for (int i = 0;i < 6;i++)
 				{
 					for (int j = 0;j < 14;j++) {
-						if (i + 1 == floor && j + 1 == select_room)
+						if (i + 1 == arr_room.floor && j + 1 == arr_room.room)
 						{
 							if (room_data[i][j] == "Booked")
 							{
 								cout << "Already booked!\n";
 							}
-							else if (room_data[floor - 1][select_room - 1] == "Booked") {
+							else if (room_data[arr_room.floor - 1][arr_room.room - 1] == "Booked") {
 								cout << "Room is already booked!\n";
 							}
 							else {
-								/*funct( i, j);*/
 								room_data[i][j] = "Booked";
 								cout << "Booking completed successfully!\n";
 								customer_booked++;
 							}
 						}
 					}
-				}			
-			}
+				}
+				for (int i = 0;i < floors - 2;i++) {
+					cout << "Floor " << i + 1 << ": ";
+					for (int j = 0;j < rooms_per_floor;j++) {
+						cout << room_data[i][j] << setw(10);
+					}
+					cout << endl;
+				}
+			}*/
 		}
 	}
 }
 string line,arr_line[3];
-void classic_book()
+void manage_guest_details()
 {
-	for (int i = 0;i < 3;i++)
-	{
-	for (int i = 0;i < current_guest + 1;i++) {
-		if (i == current_guest) {
-			cout << "Enter customer_id:" << current_guest+1<<": "<< arr_customer[current_guest].customer_id<<endl;
-			cout << "Enter customer_name" << current_guest+1<<":";
-			cin >> arr_customer[current_guest].customer_name;
-			cout << "Enter days for stay :";
-			cin >> arr_customer[current_guest].time_duration;
-			if (arr_customer[current_guest].time_duration < 1)  cout << "Sorry!Your stay should be >=1";
-			else {
-				current_record.open("current_record.txt", ios::app);
-				if (current_record.is_open()) {
-					current_record << arr_customer[i].customer_id << " " << arr_customer[i].customer_name << "\n";
-					cout << "Record saved successfully!";
-				}
-			}
-		}
-		current_record.close();
+	if (current_guest >= max_guests) {
+		cout << "Maximum guest capacity reached.\n";
 	}
-	current_guest++;
-	room_book();
-}
-void penthouse()
-{
-	cout << "Enter floors from(1-" << penthouse_floors << "): ";
-	int floor;
-	cin >> floor;
-	cout << "Enter room from(1-" << penthouse_floors << "): ";
-	int room;
-	cin >> room;
-	for (int i = 0;i < penthouse_floors;i++) 
-	{
-		for (int j = 0;j < penthouse_rooms;j++)
-		{
-			penthouse_arr[i][j].floor;
-		}
+	else {
+		customer* guest = &guests[current_guest];
+		guest->customer_id = rand() % 10000 + 1;
+		cout << "Enter customer name:";
+		cin.ignore();
+		getline(cin, guest->customer_name);
+		cout << "Enter days for stay:";
+		cin >> guest->time_duration;
+		guest->meal_charges = 0;
+		cout << "Guest details saved successfully!\n";
+		current_guest++;
 	}
 }
-void bookings()
+void checkIN_guest()
 {
-	cout << "1. Penthouse:\n";
-	cout << "2. Classic:\n";
-	int ch;
-	cin >> ch;
-	switch (ch)
-	{
-	case 1:
-		penthouse();
-	case 2:
-		classic_book();
-	}	
+	bool Isbook = false;
+	int input_Id;
+	cout << "Enter guest ID: ";
+	cin >> input_Id;
+	for (int i = 0; i < current_guest; i++) {
+		if (guests[i].customer_id == input_Id) {
+			cout << "Check-in successful for guest " << guests[i].customer_name << ".\n";
+			Isbook = true;
+		}
+	}
+	if(Isbook=false)  cout << "Guest not found.\n";	
 }
+//void classic_book()
+//{
+//			    for (int i = 0;i < 3;i++)
+//				{
+//					for (int i = 0;i < current_guest + 1;i++) {
+//						if (i == current_guest) {
+//							cout << "Your customer_id is:" << current_guest + 1 << ": " << arr_customer[current_guest].customer_id << endl;
+//							cout << "Enter customer_name" << current_guest + 1 << ":";
+//							cin >> arr_customer[current_guest].customer_name;
+//							cout << "Enter days for stay :";
+//							cin >> arr_customer[current_guest].time_duration;
+//							if (arr_customer[current_guest].time_duration < 1)  cout << "Sorry!Your stay should be atleast 1 day";
+//							else {
+//								current_record.open("current_record.txt", ios::app);
+//								if (current_record.is_open()) {
+//									current_record << arr_customer[i].customer_id << " " << arr_customer[i].customer_name << "\n";
+//									cout << "Record saved successfully!";
+//								}
+//							}
+//						}
+//						current_record.close();
+//					}
+//					room_book();
+//					/*database.open("database.txt", ios::app);
+//					if (database.is_open()) {
+//						database << arr_customer[current_guest].customer_id << " stayed at " << arr_room.floor << " in " << arr_room.room << price_per_room << endl;
+//						cout << "Record saved successfully!\n";
+//					}
+//					else cout << "File not opened!\n";
+//					d/*atabase.close();*/
+//				}
+//}
+//void penthouse()
+//{
+//	cout << "Enter floors from(1-" << penthouse_floors << "): ";
+//	int floor;
+//	cin >> floor;
+//	cout << "Enter room from(1-" << penthouse_floors << "): ";
+//	int room;
+//	cin >> room;
+//	for (int i = 0;i < penthouse_floors;i++) 
+//	{
+//		for (int j = 0;j < penthouse_rooms;j++)
+//		{
+//			penthouse_arr[i][j].floor;
+//		}
+//	}
+//}
+//void bookings()
+//{
+//	cout << "1. Penthouse:\n";
+//	cout << "2. Classic:\n";
+//	int ch;
+//	cin >> ch;
+//	switch (ch)
+//	{
+//	case 1:
+//		penthouse();
+//	case 2:
+//		classic_book();
+//	}	
+//}
 void room_delivery()
 {
 	cout << "Enter your floor:";
@@ -177,27 +234,49 @@ const int max_members = 7;
 //{
 //
 //}
-void item(int price)
+void item(string food,float price)
 {
-	cout << "Enter quantity:";
-	int quantity;
-	cin >> quantity;
-	cout << "Total amount:" << price * quantity;
+	cout << "Enter customer_ID:";
+	float total;
+	int inp_ID;
+	cin >> inp_ID;
+	for (int i = 0;i < current_guest;i++) {
+		if (guests[i].customer_id == inp_ID)
+		{
+			cout << "Enter quantity:";
+			int quantity;
+			cin >> quantity;
+			total = price * quantity;
+			guests[i].meal_charges = total;
+			cout << "Order placed successfully!\n";
+				/*current_record.open("current_record.txt", ios::in);*/
+				/*if (current_record.is_open()) {
+					while (getline(current_record, line)) {
+						if (stoi(line) == inp_ID) {
+							meal.open("meal.txt", ios::app);
+							if (meal.is_open()) {
+								meal << inp_ID << " " << food << " " << "$" << price << endl;
+							}
+						}
+					}
+				}*/
+		}
+	}
 }
-void meal()
+void meal_func()
 {
-	int price=0;
+    float price=0;
 	cout << "***Menu***\n";
-	cout << "1. Burger"<<setw(5)<<"$100"<<endl;
-	cout << "2. Pasta\n" << setw(5) << "$200" << endl;
-	cout << "3. Chicken\n" << setw(5) << "$300" << endl;
-	cout << "4. Mutton\n" << setw(5) << "$400" << endl;
-	cout << "5. Noodles\n" << setw(5) << "$100" << endl;
-	cout << "6. Platter\n" << setw(5) << "$500" << endl;
-	cout << "7. Tea\n" << setw(5) << "$50" << endl;
-	cout << "8. Snacks\n" << setw(5) << "$100" << endl;
-	cout << "9. Desserts\n" << setw(5) << "$300" << endl;
-	cout << "10. Drinks\n" << setw(5) << "$100" << endl;
+	cout << "1. Burger"<<setw(5)<<"$100.2"<<endl;
+	cout << "2. Pasta" << setw(5) << "$200.7" << endl;
+	cout << "3. Chicken" << setw(5) << "$300.9" << endl;
+	cout << "4. Mutton" << setw(5) << "$400.0" << endl;
+	cout << "5. Noodles" << setw(5) << "$100.9" << endl;
+	cout << "6. Platter" << setw(5) << "$500.1" << endl;
+	cout << "7. Tea" << setw(5) << "$50.2" << endl;
+	cout << "8. Snacks" << setw(5) << "$100.0" << endl;
+	cout << "9. Desserts" << setw(5) << "$300.8" << endl;
+	cout << "10. Drinks" << setw(5) << "$100.5" << endl;
 	int select;
 	string food;
 	cout << "Selection:";
@@ -205,42 +284,42 @@ void meal()
 	switch (select)
 	{ 
 	case 1:
-		food = burger;
-		price = 100;
+		food = "burger";
+		price = 100.2;
 		break;
 	case 2:
-		food = pasta;
-		price = 200;
+		food = "pasta";
+		price = 200.7;
 		break;
 	case 3:
-		food = chicken;
-		price = 300;
+		food = "chicken";
+		price = 300.9;
 		break;
 	case 4: 
-		price = 400;
+		price = 400.0;
 		break;
 	case 5:
-		price = 100;
+		price = 100.9;
 		break;
 	case 6:
-		price = 500;
+		price = 500.1;
 		break;
 	case 7:
-		price = 50;
+		price = 50.2;
 		break;
 	case 8:
-		price = 100;
+		price = 100.0;
 		break;
 	case 9:
-		price = 300;
+		price = 300.8;
 		break;
 	case 10:
-		price = 100;
+		price = 100.5;
 		break;
 	default:
 		break;
 	}
-	item(price);
+	item(food,price);
 }
 void history_funct(customer c)
 {
@@ -269,7 +348,7 @@ void meal_options()
 	//	/*dining_table();*/
 	//case 3:
 	}
-	meal();
+	meal_func();
 }
 void delete_record(customer c)
 {
@@ -286,7 +365,15 @@ void check_out()
 	cout << "Enter customer ID for check out:";
 	int input_ID;
 	cin >> input_ID;
-	string line;
+	for (int i = 0;i < current_guest;i++) {
+		if (guests[i].customer_id == input_ID) {
+			guests[i].time_duration=0;
+			guests[i].meal_charges = 0;
+			cout << "Check-out successful for guest " << guests[i].customer_name << ".\n";
+			guests[i].customer_name.clear();
+		}
+	}
+	/*string line;
 	customer c;
 	current_record.open("current_record.txt", ios::in);
 	if (current_record.is_open()) {
@@ -306,18 +393,26 @@ void check_out()
 	const char* current_record = "current_record.txt";
 	const char* current_record_new = "current_record_new.txt";
 	remove("current_record.txt");
-	rename("current_record.txt", "current_record_new.txt");
+	rename("current_record.txt", "current_record_new.txt");*/
 }
-void bill(int customer_id, int stay_duration, float room_rate, float meal_charge, float service_fee)
+void bill()
 {
-	float total = (stay_duration * room_rate) + meal_charge + service_fee;
-	cout << "Customer ID:" << customer_id << endl;
-	cout << "Stay duration:" << stay_duration << endl;
-	cout << "Room rate per room:" << room_rate << endl;
-	cout << "Total meal charge:" << meal_charge << endl;
-	cout << "Additional service fee:" << service_fee<< endl;
-	cout << "--------------\n";
-	cout << "Total Bill :"<<total<<endl;
+	cout << "Enter customer_id:";
+	int inp_id;
+	cin>> inp_id;
+	for (int i = 0;i < current_guest;i++)
+	{
+		if (guests[i].customer_id == inp_id) {
+			cout << "Customer ID:" << guests[i].customer_id << endl;
+			cout << "Stay duration:" << guests[i].time_duration<< endl;
+			cout << "Room rate per room:" << room_rate << endl;
+			cout << "Total meal charge:" << guests[i].meal_charges << endl;
+			cout << "Additional service fee:" << service_fee << endl;
+			cout << "--------------\n";
+			float total = guests[i].time_duration * room_rate + guests[i].meal_charges + service_fee;
+			cout << "Total Bill :" << total << endl;
+		}
+	}    
 }
 void guest()
 {
@@ -335,6 +430,7 @@ void guest()
 		cout << "9. Manage Special Requests\n";
 		cout << "10. Log Out\n";
 		cout << "Enter your services:";
+		initializerooms();
 		int services;
 		cin >> services;
 		switch (services)
@@ -343,18 +439,22 @@ void guest()
 			check_room_availability();
 			break;
 		case 2:
-			bookings();
+			room_book();
 			break;
-		/*case 3:*/
-
+		case 3:
+			manage_guest_details();
+			break;
 		case 4:
 			meal_options();
+			break;
+		case 5:
+			checkIN_guest();
 			break;
 		case 6:
 			check_out();
 			break;
-		case 7:
-			bill(customer_id, stay_duration, room_rate, meal_charge, service_fee);
+		//case 7:
+			/*bill(customer_id, stay_duration, room_rate, meal_charge, service_fee);*/
 		}
 		cout << "Press any key to continue :";
 		cin >> cont;
@@ -375,6 +475,4 @@ int main()
 	case 2:
 		guest();
 	}
-
 }
-
